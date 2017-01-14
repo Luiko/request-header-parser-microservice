f97781e35aa60e5bb7bb3eccfb7286d70a4f5230
Luiko <lokoluis_box@hotmail.com>
2017-01-14 14:09:48 -0700

    fix test

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ app.use(function (req, res) {
     var obj = {};
     obj['IP address'] = req.headers['x-forwarded-for'] || req.connection.remoteAddress || 
         req.socket.remoteAddress || req.ip;
-    obj.language = req.headers['accept-language'].split(',')[0];
-    obj['operating system'] = req.headers['user-agent'].split(/[\(\)]/)[1];
-    res.send(JSON.stringify(obj));
+    if (req.headers['accept-language']) {
+        obj.language = req.headers['accept-language'].split(',')[0];
+    }
+    if (req.headers['user-agent']) {
+        obj['operating system'] = req.headers['user-agent'].split(/[\(\)]/)[1];   
+    }
+    //res.writeHead(200, { 'Content-Type': 'application/json' });
+    //res.end(JSON.stringify(obj));
+
+    //res.setHeader('Content-Type', 'application/json');
+    //res.send(JSON.stringify(obj));
+
+    res.json(obj);
 });
 app.listen(app.get('port'), function () {
     console.log('Server is listening on port', app.get('port'));
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var http = require('http');
 var os = require('os');
-var getIP = require('external-ip');
+var getIP = require('external-ip')();
 test('request test', function (t) {
     t.plan(3);
     var request = http.request( {
@@ -9,18 +9,18 @@ test('request test', function (t) {
         port: 8080,
         method: 'GET'
     }, function (res) {
-        res.setEncoding('utf8');
         var data = "";
         res.on('data', function (chunk) {
-            data +="chunk";
+            data += chunk;
         });
         res.on('end', function () {
             var obj = JSON.parse(data);            
-            t.equal(os.type, obj['operating system']);
-            t.equal('en', obj.language);
+            t.equal(obj['operating system'], os.type);
+            t.equal(obj.language, 'en-US');
             getIP(function (err, ip) {
                 if (err) return console.error(err);
-                t.equal(ip, obj['IP address']);
+                console.log(obj);
+                t.equal(obj['IP address'], ip);
             });
         });
         res.on('error', function (err) {
